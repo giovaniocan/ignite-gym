cde6dac847c0f821b5cb5d27d3f3d75eed7eed0c
giovani <giovaniocan@gmail.com>
2023-08-29 15:28:44 -0300

    added theme using native-base

--- App.tsx
+++ App.tsx
@@ -1,22 +1,25 @@
-import { Text, View, StatusBar } from 'react-native';
+import {  View, StatusBar } from 'react-native';
 
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { NativeBaseProvider } from 'native-base';
+import { Loading } from '@components/Loading';
+import {THEME} from './src/theme'
 
 
 export default function App() {
  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
 
   return (
-    <View style={{flex:1, alignItems:'center', justifyContent:'center', backgroundColor:"#202024"}}>
+    <NativeBaseProvider theme={THEME} >
 
       <StatusBar
-        barStyle="light-content"
-        backgroundColor="trasnparent"
-        translucent
+       barStyle="light-content"
+       backgroundColor={"transparent"}
+       translucent 
       />
 
-      {fontsLoaded ? <Text >Hello World</Text> : <View/>}
+     {fontsLoaded ? <View/> : <Loading/> }
       
-    </View>
+    </NativeBaseProvider>
   );
 }
