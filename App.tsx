638929d87048a98a9bb330abd73551e0ab9a3568
giovani <giovaniocan@gmail.com>
2023-08-29 14:32:33 -0300

    Created project

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native';
+
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+
+export default function App() {
+ const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
+  return (
+    <View style={{flex:1, alignItems:'center', justifyContent:'center', backgroundColor:"#fff"}}>
+      {fontsLoaded ? <Text >Hello World</Text> : <View/>}
+      
+    </View>
+  );
+}
