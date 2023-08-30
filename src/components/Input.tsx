6aef20b6b738c4221ce0d5d86313fab8a0a383fe
giovani <giovaniocan@gmail.com>
2023-08-30 14:44:53 -0300

    Inputs is done

---
+++ src/components/Input.tsx
@@ -0,0 +1,28 @@
+import {Input as NativeInput, IInputProps} from 'native-base'
+import { border } from 'native-base/lib/typescript/theme/styled-system'
+
+
+
+export function Input({...rest}:IInputProps){
+    return(
+        <NativeInput
+            bg="gray.700"
+            h={14}
+            px={4}
+            borderWidth={0}
+            fontSize={'md'}
+            color="white"
+            fontFamily="body"
+            mb={4}
+            placeholderTextColor="gray.300"
+            borderRadius={8}
+            _focus={{
+                bg:"gray.700",
+                borderWidth:1,
+                borderColor:"green.500",
+            }}
+
+            {...rest}
+        />
+    )
+}
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,19 +1,21 @@
-import { Center, Image, Text, VStack } from "native-base";
+import { Center, Heading, Image, Text, VStack } from "native-base";
 
 import LogoSvg from '@assets/logo.svg'
 
 
 
 import BackgroundImg from '@assets/background.png'
+import { Input } from "@components/Input";
 
 export function SignIn(){
     return(
-        <VStack flex={1} bg="gray.700" >
+        <VStack flex={1} bg="gray.700" px={10} >
             <Image
                 source={BackgroundImg} 
                 alt="People working out"
                 resizeMode="contain"
                 position={"absolute"}
+                opacity={85}
             />
 
             <Center my={"24"}>
@@ -23,6 +25,22 @@ export function SignIn(){
                 </Text>
             </Center>
 
+            <Center>
+                <Heading color="gray.100" fontSize="xl" mb="6" fontFamily="heading">
+                    Access your account
+                </Heading>
+            </Center>
+
+            <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+            <Input
+                placeholder="Password"
+                secureTextEntry
+            />
             
 
         </VStack>
