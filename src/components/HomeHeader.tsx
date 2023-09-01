f9e01e54e3ed966fa1696f364d00cabbdf43d149
giovani <giovaniocan@gmail.com>
2023-09-01 15:20:41 -0300

    finished HomeHeader component

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,9 +1,22 @@
-import { HStack, Heading, Text, VStack } from "native-base";
+import { HStack, Heading, Text, VStack, Icon } from "native-base";
+import { MaterialIcons } from '@expo/vector-icons'
+
+import { UserPhoto } from "./UserPhoto";
+import { TouchableOpacity } from "react-native";
 
 export function HomeHeader(){
     return(
-        <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
-            <VStack>
+        <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center"     >
+            <UserPhoto 
+                source={{
+                    uri: "https://github.com/giovaniocan.png"
+                }}
+                size={16}
+                mr={4}
+                alt="your profile photo"
+            />  
+
+            <VStack flex={1}>
                 <Text color="gray.100" fontSize="md">
                     Hello,
                 </Text>
@@ -12,6 +25,17 @@ export function HomeHeader(){
                     Giovani
                 </Heading>
             </VStack>
+
+            <TouchableOpacity>
+                <Icon
+                    as={MaterialIcons}
+                    name="logout"
+                    color="gray.200"
+                    size={7}
+                />
+            </TouchableOpacity>
+
+            
         </HStack>
     )
 }
