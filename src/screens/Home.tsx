4304e18a99522d5435cdd8b2d656e6fc7ca6ef0f
giovani <giovaniocan@gmail.com>
2023-09-04 18:31:27 -0300

    created Group component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,30 @@
-import { HomeHeader } from "@components/HomeHeader";
-import {  VStack } from "native-base";
 
-export function Home(){
-    return(
-        <VStack flex={1}>
-            <HomeHeader />
-        </VStack>
-    )
+import { Group } from '@components/Group';
+import { HomeHeader } from '@components/HomeHeader';
+import { HStack, VStack } from 'native-base';
+import { useState } from 'react';
+
+export function Home() {
+
+  const [groupSelected, setGroupSelected] = useState('costa')
+
+  return (
+    <VStack flex={1}>
+      <HomeHeader />
+
+      <HStack>
+        <Group 
+          name="costa" 
+          isActive={groupSelected === 'costa'}
+          onPress={() => setGroupSelected('costa')}
+        />
+        <Group 
+          name="ombro" 
+          isActive={groupSelected === 'ombro'}
+          onPress={() => setGroupSelected('ombro')}
+        />
+      </HStack>
+
+    </VStack>
+  )
 }
